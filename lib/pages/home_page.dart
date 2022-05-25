b7ccba65ed26ea56d9004ea0aeef055e6f25d387
Alexeis Vladimir Carrillo Pinaya <acarrillo@acarrillou.ruat.net>
2022-05-25 16:09:57 -0400

    adecuaciones sqflite

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/pages/direcciones_page.dart';
+import 'package:qr_reader/pages/mapas_page.dart';
+import 'package:qr_reader/providers/db_provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
+import 'package:qr_reader/providers/ui_provider.dart';
 import 'package:qr_reader/widgets/custom_navigatorbar.dart';
 import 'package:qr_reader/widgets/scann_button.dart';
 
@@ -18,12 +24,36 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
-        child: Text('Home Page'),
-      ),
+      body: _HomePageBody(),
       bottomNavigationBar: CustomNavigationBar(),
       floatingActionButton: ScanButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    //Usar el Scanlistprovider
+    final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
+    
+
+    switch (currentIndex) {
+      case 0:
+        scanListProvider.cargarScanPorTipo('geo');
+        return MapasPage();
+      case 1:
+        scanListProvider.cargarScanPorTipo('http');
+        return DireccionesPage();
+      default:
+        scanListProvider.cargarScanPorTipo('geo');
+        return MapasPage();
+    }
   }
 }
--- lib/widgets/scann_button.dart
+++ lib/widgets/scann_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_list_provider.dart';
 class ScanButton extends StatelessWidget {
   const ScanButton({Key? key}) : super(key: key);
 
@@ -8,7 +10,18 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       elevation: 0,
       child: Icon(Icons.filter_center_focus),
-      onPressed: () {}
+      onPressed: () async {
+        /*String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+                                                    '#3D8BEF', 
+                                                    'Cancelar', 
+                                                    false, 
+                                                    ScanMode.QR);*/
+        final barcodeScanRes = 'https://alexeis.yvaganet.con';
+        final scanListProvider = Provider.of<ScanListProvider>(context, listen: false);
+        scanListProvider.nuevoScan(barcodeScanRes);
+        scanListProvider.nuevoScan('geo:15.33,15.66');
+        print(barcodeScanRes);
+      }
     );
   }
 }
